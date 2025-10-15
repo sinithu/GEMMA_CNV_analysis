7d28197cd345285d3f4139f22316608e74ae34ec
Sini <222218984+sinithu@users.noreply.github.com>
2025-10-15 10:25:16 +0300

    Update genes_biotypes_files.sh

--- downstream_analysis/enrichment_analysis/genes_biotypes_files.sh
+++ downstream_analysis/enrichment_analysis/genes_biotypes_files.sh
@@ -7,7 +7,7 @@ bcftools view -S /path/controls.txt /path/merged/samples_truvari_vep.vcf.gz -Oz 
 
 #Creating files of gene information (VEP columns "SYMBOL" and "BIOTYPE") separately in deletions and duplications
 #COUNT(CN!=".")>0 is used to include only actual variants (i.e. variants with copy number information in at least one sample)
-bcftools view -i 'ALT="<DEL>" & COUNT(CN!=".")>0' samples_truvari_vep_cases.vcf.gz | bcftools +split-vep -f'%SYMBOL\t%BIOTYPE\n' > genes_biotype_DEL_cases.txt
-bcftools view -i 'ALT="<DUP>" & COUNT(CN!=".")>0' samples_truvari_vep_cases.vcf.gz | bcftools +split-vep -f'%SYMBOL\t%BIOTYPE\n' > genes_biotype_DUP_cases.txt
-bcftools view -i 'ALT="<DEL>" & COUNT(CN!=".")>0' samples_truvari_vep_controls.vcf.gz | bcftools +split-vep -f'%SYMBOL\t%BIOTYPE\n' > genes_biotype_DEL_controls.txt
-bcftools view -i 'ALT="<DUP>" & COUNT(CN!=".")>0' samples_truvari_vep_controls.vcf.gz | bcftools +split-vep -f'%SYMBOL\t%BIOTYPE\n' > genes_biotype_DUP_controls.txt
+bcftools view -i 'ALT="<DEL>" & COUNT(CN!=".")>0' samples_truvari_vep_cases.vcf.gz | bcftools +split-vep -f'%SYMBOL\t%BIOTYPE\n' > /path/genes_biotype_DEL_cases.txt
+bcftools view -i 'ALT="<DUP>" & COUNT(CN!=".")>0' samples_truvari_vep_cases.vcf.gz | bcftools +split-vep -f'%SYMBOL\t%BIOTYPE\n' > /path/genes_biotype_DUP_cases.txt
+bcftools view -i 'ALT="<DEL>" & COUNT(CN!=".")>0' samples_truvari_vep_controls.vcf.gz | bcftools +split-vep -f'%SYMBOL\t%BIOTYPE\n' > /path/genes_biotype_DEL_controls.txt
+bcftools view -i 'ALT="<DUP>" & COUNT(CN!=".")>0' samples_truvari_vep_controls.vcf.gz | bcftools +split-vep -f'%SYMBOL\t%BIOTYPE\n' > /path/genes_biotype_DUP_controls.txt
